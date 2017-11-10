86b7669778ca9125a499cff31d8327af307ed631
j-christoffersen <jacksontchristo@gmail.com>
2017-11-09 16:06:53 -0800

    fix bug

--- server/index.js
+++ server/index.js
@@ -49,9 +49,9 @@ app.get('/repos', function (req, res) {
   })
 });
 
-let port = 1128;
+let port = process.env.PORT || 1128;
 
 app.listen(port, function() {
-  console.log(`listening on port ${process.env.$PORT || port}`);
+  console.log(`listening on port ${port}`);
 });
 
